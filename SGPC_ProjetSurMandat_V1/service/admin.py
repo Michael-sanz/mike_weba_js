00c88347b7f5d8d9196ad4269a5fe7cc47ff566b
michael <elv-michael.snz@eduge.ch>
2021-02-08 17:37:28 +0100

    Initial commit

---
+++ SGPC_ProjetSurMandat_V1/service/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+admin.site.register(SGPC_RESERVATION)
+admin.site.register(SGPC_SERVICE)
+admin.site.register(SGPC_DEVIS)
+# admin.site.register(SGPC_PROVIENT)
+admin.site.register(SGPC_ASSO_SER_DEV)
+# admin.site.register(FAIT_REFERENCE_A)
